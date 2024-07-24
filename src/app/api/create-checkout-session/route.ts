61c3782b1880173de7fd9529be7c862ef8ef1934
Ashutosh Yadav <ashutosh.js@zestgeek.com>
2024-07-24 11:48:47 +0530

    added domain

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -68,8 +68,8 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       ],
       mode: "subscription",
       customer: session.user.stripeCustomerId,
-      success_url: `https://copyin2clicks.vercel.app/?success=true&session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `https://copyin2clicks.vercel.app/?canceled=true`,
+      success_url: `https://www.copyin2clicks.com/?success=true&session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://www.copyin2clicks.com/?canceled=true`,
       subscription_data: {
         metadata: {
           payingUserId: session.user.id,
